ea2b00104441fe0d433220ee10a640628afd2a2c
arcbjorn <arky.happ@gmail.com>
2022-11-02 22:00:22 -0300

    feat(internal/router): add CRUD logic

--- internal/router.go
+++ internal/router.go
@@ -8,8 +8,18 @@ type Router struct {
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
+	case "/":
+		w.WriteHeader(http.StatusOK)
 	case "/all":
 		router.Handler.GetAll(w, r)
+	case "/get":
+		router.Handler.GetOne(w, r)
+	case "/create":
+		router.Handler.Create(w, r)
+	case "/update":
+		router.Handler.Update(w, r)
+	case "/delete":
+		router.Handler.Delete(w, r)
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 	}

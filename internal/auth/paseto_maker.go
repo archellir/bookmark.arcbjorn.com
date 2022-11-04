dbf26be3a59cfd1586ea68ca02bd91dff6223199
arcbjorn <arky.happ@gmail.com>
2022-11-04 03:21:11 -0300

    feat(auth): implement paseto_maker

---
+++ internal/auth/paseto_maker.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/o1egl/paseto"
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type PasetoMaker struct {
+	paseto       *paseto.V2
+	symmetricKey []byte
+}
+
+func NewPasetoMaker(symmetricKey string) (IMaker, error) {
+	if len(symmetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:       paseto.NewV2(),
+		symmetricKey: []byte(symmetricKey),
+	}
+
+	return maker, nil
+}
+
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewToken(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+func (maker *PasetoMaker) VerifyToken(salt string) (*Token, error) {
+	token := &Token{}
+
+	err := maker.paseto.Decrypt(salt, maker.symmetricKey, token, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = token.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}

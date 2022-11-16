5d3095e62ec6df1dbb733bb25b3e991d054e94b4
arcbjorn <arky.happ@gmail.com>
2022-11-16 19:08:25 -0300

    feat(cmd/main): add error handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"log"
+
 	api "github.com/archellir/bookmark.arcbjorn.com/api"
 )
 
 func main() {
-	server := api.NewServer()
+	server, err := api.NewServer()
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
 	server.Start()
 }

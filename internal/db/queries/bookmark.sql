c643cdb68a2f704cf46a86d51f01b15ea7565dd8
arcbjorn <arky.happ@gmail.com>
2022-11-17 13:43:30 -0300

    feat(db/queries): bookmark CRUD & Full Tex tSearch

---
+++ internal/db/queries/bookmark.sql
@@ -0,0 +1,47 @@
+-- name: CreateBookmark :one
+INSERT INTO bookmarks (
+  name,
+  search_tokens,
+  url
+) VALUES (
+  $1, to_tsvector($1), $2
+) RETURNING *;
+
+-- name: GetBookmarkById :one
+SELECT * FROM bookmarks
+WHERE id = $1 LIMIT 1;
+
+-- name: GetBookmark :one
+SELECT * FROM bookmarks
+WHERE id = $1 LIMIT 1;
+
+-- name: ListBookmarks :many
+SELECT * FROM bookmarks
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateBookmarkName :one
+UPDATE bookmarks
+SET name = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: UpdateBookmarkUrl :one
+UPDATE bookmarks
+SET url = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: SearchBookmarkByNameAndUrl :many
+SELECT * FROM bookmarks  
+WHERE
+  url LIKE '%' + sqlc.arg(search_string)::text + '%' OR
+  name @@ to_tsquery(sqlc.arg(search_string)::text);
+
+-- name: DeleteBookmark :exec
+DELETE FROM bookmarks
+WHERE id = $1;
+
+-- name: DeleteBookmarks :exec
+DELETE FROM bookmarks;

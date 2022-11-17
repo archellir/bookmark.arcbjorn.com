51bb95d2cfa71b327db7ca945d6792a039ae5d78
arcbjorn <arky.happ@gmail.com>
2022-11-17 18:15:39 -0300

    feat(transport/handlers): add tags

---
+++ internal/transport/handlers/tags.go
@@ -0,0 +1,69 @@
+package transport
+
+import (
+	"net/http"
+)
+
+type TagHandler struct{}
+
+func (handler *TagHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/tags/all":
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handler.GetAll(w, r)
+
+	case "/tags/get":
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handler.GetOne(w, r)
+
+	case "/tags/create":
+		if r.Method != "POST" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handler.Create(w, r)
+
+	case "/tags/update":
+		if r.Method != "PUT" && r.Method != "PATCH" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handler.Update(w, r)
+
+	case "/tags/delete":
+		if r.Method != "DELETE" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		handler.Delete(w, r)
+
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func (handler *TagHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("all tags"))
+}
+
+func (handler *TagHandler) GetOne(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("one tag"))
+}
+
+func (handler *TagHandler) Create(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("created tag"))
+}
+
+func (handler *TagHandler) Update(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("updated tag"))
+}
+
+func (handler *TagHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("true"))
+}

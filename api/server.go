f36f2fab08988e17199b7a898080a07657138ac1
arcbjorn <arky.happ@gmail.com>
2022-11-17 22:41:55 -0300

    feat(api/server): use InitStore

--- api/server.go
+++ api/server.go
@@ -5,16 +5,20 @@ import (
 	"net/http"
 
 	auth "github.com/archellir/bookmark.arcbjorn.com/internal/auth"
+	orm "github.com/archellir/bookmark.arcbjorn.com/internal/db/orm"
 	transport "github.com/archellir/bookmark.arcbjorn.com/internal/transport"
 	handlers "github.com/archellir/bookmark.arcbjorn.com/internal/transport/handlers"
 )
 
 type Server struct {
 	Router     *transport.Router
+	Store      *orm.Store
 	tokenMaker auth.IMaker
 }
 
 func NewServer() (*Server, error) {
+	store := orm.InitStore()
+
 	tokenMaker, err := auth.NewPasetoMaker("12345678901234567890123456789012")
 	if err != nil {
 		return nil, fmt.Errorf("cannot create token maker: %w", err)
@@ -32,6 +36,7 @@ func NewServer() (*Server, error) {
 
 	server := &Server{
 		Router:     router,
+		Store:      store,
 		tokenMaker: tokenMaker,
 	}
 
@@ -41,5 +46,5 @@ func NewServer() (*Server, error) {
 func (server *Server) Start() {
 	// addr := fmt.Sprint("localhost:", os.Getenv("SERVER_PORT"))
 
-	http.ListenAndServe(":8080", server.Router)
+	http.ListenAndServe("localhost:8080", server.Router)
 }

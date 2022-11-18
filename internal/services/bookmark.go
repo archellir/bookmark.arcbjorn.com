82c384303d7940123929653aa110a38483b4f96b
arcbjorn <arky.happ@gmail.com>
2022-11-18 01:40:27 -0300

    feat(internal/services/bookmark): add List logic

--- internal/services/bookmark.go
+++ internal/services/bookmark.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	orm "github.com/archellir/bookmark.arcbjorn.com/internal/db/orm"
 )
@@ -10,8 +14,51 @@ type BookmarkService struct {
 	Store *orm.Store
 }
 
-func (service *BookmarkService) List(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("all bookmarks"))
+func (service *BookmarkService) List(w http.ResponseWriter, r *http.Request) error {
+	limit := defaultLimit
+	offset := defaultOffset
+
+	if r.URL.Query().Has(limitParamName) {
+		limitParam := r.URL.Query().Get(limitParamName)
+		parsedInt, err := strconv.Atoi(limitParam)
+		if err != nil {
+			return fmt.Errorf("error parsing list limit")
+		}
+		limit = parsedInt
+	}
+
+	if r.URL.Query().Has(offsetParamName) {
+		offsetParam := r.URL.Query().Get(offsetParamName)
+		parsedInt, err := strconv.Atoi(offsetParam)
+		if err != nil {
+			return fmt.Errorf("error parsing list offset")
+		}
+		offset = parsedInt
+	}
+
+	args := &orm.ListBookmarksParams{
+		Limit:  int32(limit),
+		Offset: int32(offset),
+	}
+
+	result, err := service.Store.Queries.ListBookmarks(context.Background(), *args)
+	if err != nil {
+		return fmt.Errorf("can not retrieve bookmarks: %w", err)
+	}
+
+	if len(result) == 0 {
+		result = []orm.Bookmark{}
+	}
+
+	json, err := json.Marshal(result)
+	if err != nil {
+		return fmt.Errorf("can not generate json: %w", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+
+	return nil
 }
 
 func (service *BookmarkService) GetOne(w http.ResponseWriter, r *http.Request) {

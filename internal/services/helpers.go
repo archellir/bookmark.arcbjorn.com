c63e482272baa73b6b4b1267d2a02a2a3e29e471
arcbjorn <arky.happ@gmail.com>
2022-11-18 10:53:30 -0300

    feat(internal/services/helpers): add GetListParams

---
+++ internal/services/helpers.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+)
+
+const (
+	limitParamName  = "limit"
+	offsetParamName = "offset"
+)
+
+const (
+	defaultLimit  int32 = 25
+	defaultOffset int32 = 0
+)
+
+func GetListParams(url *url.URL) (limit int32, offset int32, err error) {
+	limit = defaultLimit
+	offset = defaultOffset
+
+	if url.Query().Has(limitParamName) {
+		limitParam := url.Query().Get(limitParamName)
+		parsedInt, err := strconv.Atoi(limitParam)
+		if err != nil {
+			return 0, 0, fmt.Errorf("error parsing list limit")
+		}
+		limit = int32(parsedInt)
+	}
+
+	if url.Query().Has(offsetParamName) {
+		offsetParam := url.Query().Get(offsetParamName)
+		parsedInt, err := strconv.Atoi(offsetParam)
+		if err != nil {
+			return 0, 0, fmt.Errorf("error parsing list offset")
+		}
+		offset = int32(parsedInt)
+	}
+
+	return limit, offset, nil
+}

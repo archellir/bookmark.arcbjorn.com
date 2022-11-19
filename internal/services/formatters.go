6d017a31ec133036729d60257d1352f04009b705
arcbjorn <arky.happ@gmail.com>
2022-11-18 22:20:11 -0300

    feat(int/services/formatters): Int32ToSqlNullInt32

--- internal/services/formatters.go
+++ internal/services/formatters.go
@@ -1,8 +1,22 @@
 package services
 
 import (
+	"database/sql"
+
 	orm "github.com/archellir/bookmark.arcbjorn.com/internal/db/orm"
 )
+
+func Int32ToSqlNullInt32(n int32) *sql.NullInt32 {
+	valid := true
+	if n == 0 {
+		valid = false
+	}
+
+	return &sql.NullInt32{
+		Int32: n,
+		Valid: valid,
+	}
+}
 
 func FormatBookmark(bookmark orm.Bookmark) *tFormattedBookmark {
 	return &tFormattedBookmark{

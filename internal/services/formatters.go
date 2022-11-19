653e99cbb436f392f25df17219ee0e0220af8180
arcbjorn <arky.happ@gmail.com>
2022-11-18 21:50:28 -0300

    ref(internal/services/formatters): clean-up

--- internal/services/formatters.go
+++ internal/services/formatters.go
@@ -4,17 +4,21 @@ import (
 	orm "github.com/archellir/bookmark.arcbjorn.com/internal/db/orm"
 )
 
-func FormatBookmarks(bookmarks []orm.Bookmark) []tFormattedBookmark {
-	var formattedBookmarks []tFormattedBookmark
+func FormatBookmark(bookmark orm.Bookmark) *tFormattedBookmark {
+	return &tFormattedBookmark{
+		ID:        bookmark.ID,
+		Name:      bookmark.Name,
+		Url:       bookmark.Url,
+		GroupID:   bookmark.GroupID.Int32,
+		CreatedAt: bookmark.CreatedAt,
+	}
+}
+
+func FormatBookmarks(bookmarks []orm.Bookmark) []*tFormattedBookmark {
+	var formattedBookmarks []*tFormattedBookmark
 
 	for idx, bookmark := range bookmarks {
-		formattedBookmarks[idx] = tFormattedBookmark{
-			ID:        bookmark.ID,
-			Name:      bookmark.Name,
-			Url:       bookmark.Url,
-			GroupID:   bookmark.GroupID.Int32,
-			CreatedAt: bookmark.CreatedAt,
-		}
+		formattedBookmarks[idx] = FormatBookmark(bookmark)
 	}
 
 	return formattedBookmarks

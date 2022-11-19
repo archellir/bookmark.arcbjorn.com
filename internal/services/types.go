f61b602c41b71723d3e43b019a10a4c5694e5b16
arcbjorn <arky.happ@gmail.com>
2022-11-19 09:28:12 -0300

    feat(internal/services/type):tUpdateBookmarkParams

--- internal/services/types.go
+++ internal/services/types.go
@@ -2,6 +2,13 @@ package services
 
 import "time"
 
+type tUpdateBookmarkParams struct {
+	ID      int32  `json:"id"`
+	Name    string `json:"name"`
+	Url     string `json:"url"`
+	GroupID int32  `json:"group_id"`
+}
+
 type tFormattedBookmark struct {
 	ID        int32     `json:"id"`
 	Name      string    `json:"name"`

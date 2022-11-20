8a258bf8a05b758617c4cddd7f54df2b2ffe24db
arcbjorn <arky.happ@gmail.com>
2022-11-20 00:24:30 -0300

    feat(internal/transport/router): use User

--- internal/transport/router.go
+++ internal/transport/router.go
@@ -12,6 +12,7 @@ type Router struct {
 	Bookmarks handlers.BookmarkHandler
 	Tags      handlers.TagHandler
 	Groups    handlers.GroupHandler
+	Users     handlers.UserHandler
 }
 
 var (
@@ -19,6 +20,7 @@ var (
 	isBookmark    = regexp.MustCompile(`^/bm`)
 	isTag         = regexp.MustCompile(`^/tags`)
 	isGroup       = regexp.MustCompile(`^/groups`)
+	isUser        = regexp.MustCompile(`^/usr`)
 )
 
 func NewRouter(store *orm.Store) *Router {
@@ -26,6 +28,7 @@ func NewRouter(store *orm.Store) *Router {
 		Bookmarks: *handlers.NewBookmarkHandler(store),
 		Tags:      *handlers.NewTagHandler(store),
 		Groups:    *handlers.NewGroupHandler(store),
+		Users:     *handlers.NewUserHandler(store),
 	}
 
 	return router
@@ -41,6 +44,8 @@ func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		router.Tags.Handle(w, r)
 	case isGroup.MatchString(r.URL.Path):
 		router.Groups.Handle(w, r)
+	case isUser.MatchString(r.URL.Path):
+		router.Users.Handle(w, r)
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 	}

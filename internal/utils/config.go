0491385a8920981f6ff62bfa03b98980b955c51d
arcbjorn <arky.happ@gmail.com>
2022-11-20 12:44:21 -0300

    feat(utils/config): add variables

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,11 +1,17 @@
 package utils
 
-import "github.com/spf13/viper"
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
-	DatabaseDriver string `mapstructure:"DATABASE_DRIVER"`
-	DatabaseSource string `mapstructure:"DATABASE_SOURCE"`
-	ServerAddress  string `mapstructure:"SERVER_ADDRESS"`
+	DatabaseDriver      string        `mapstructure:"DATABASE_DRIVER"`
+	DatabaseSource      string        `mapstructure:"DATABASE_SOURCE"`
+	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey   string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
